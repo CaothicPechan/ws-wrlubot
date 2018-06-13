ce53348d52ec3922a9a71474ca33b0b9d9de2fbc
Rafael Ulises Mendoza Reyes <rumendoza@localhost.localdomain>
2018-06-13 12:38:21 -0500

    new branch

--- js/libs/settings.js
+++ js/libs/settings.js
@@ -10,6 +10,7 @@ export default (app, constants) => {
 
     app.set('port', (process.env.PORT || port));
 
+    // App Verifying
     // app.use(bodyParser.json({
     //     verify: fbService.verifyRequestSignature
     // }));
