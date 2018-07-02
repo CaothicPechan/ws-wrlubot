374b2b3ec8bdc6a591489897d9918c5298418635
Rafael Ulises Mendoza Reyes <rumendoza@localhost.localdomain>
2018-07-02 11:30:09 -0500

    User Fix

--- js/app.js
+++ js/app.js
@@ -92,11 +92,11 @@ function setSessionAndUser(senderID) {
 	if (!sessionIds.has(senderID)) {
 		sessionIds.set(senderID, uuid.v1());
 	}
-	if (!usersMap.has(senderID)) {
-		userService.addUser(function(user){
-			usersMap.set(senderID, user);
-		}, senderID);
-	}
+	// if (!usersMap.has(senderID)) {
+	// 	userService.addUser(function(user){
+	// 		usersMap.set(senderID, user);
+	// 	}, senderID);
+	// }
 }
 
 function receivedMessage(event) {
