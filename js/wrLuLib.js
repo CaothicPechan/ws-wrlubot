54ccd8176296eae676d350d27491a27a275fe31e
Rafael Ulises Mendoza Reyes <rumendoza@localhost.localdomain>
2018-07-04 14:50:06 -0500

    redux tu wrlu lib fix 2

--- js/wrLuLib.js
+++ js/wrLuLib.js
@@ -9,7 +9,7 @@ export default class {
     constructor(app, config){
         this.app = app;
         this.fbService = new fbProvider(config.fb.graphMsgURL, config.fb.pageToken, config.fb.appSecret, config.fb.verifyToken);
-        this.dfService = new dfProvider(config.googleProjectId, fbService);
+        this.dfService = new dfProvider(config.googleProjectId, this.fbService);
         this.sessionIds = new Map();
         this.webhookUri = config.webhookUri ? config.webhookUri : '/webhook/';
 
