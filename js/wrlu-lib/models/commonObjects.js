024f6fb5d5f12de9951a62acc2852279c3bb45f8
Rafael Ulises Mendoza Reyes <rumendoza@localhost.localdomain>
2018-07-31 10:56:57 -0500

    new structure

---
+++ js/wrlu-lib/models/commonObjects.js
@@ -0,0 +1,17 @@
+export var globalResponse = {
+    status: 'success',
+    code: 200,
+    payload: '',
+    origin: '',
+    default: ''
+}
+
+
+export var wrResponse = {
+    status: 'success',
+    code: 200,
+    origin: 'facebook',
+    eventType: '',
+    payload: ''
+}
+
